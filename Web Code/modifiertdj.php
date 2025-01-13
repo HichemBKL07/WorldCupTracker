55e344543e44b5a3f9d5de4e09a9035bc9dc0ad9
MRTTBZ61 <murat.tbz@gmail.com>
2025-01-13 18:38:51 +0100

    Update modifiertdj.php

--- Web Code/modifiertdj.php
+++ Web Code/modifiertdj.php
@@ -9,14 +9,14 @@ try {
         $stmt = $base->prepare($sql);
         $stmt->execute(['id' => $id_temps_de_jeu]);
 
-        if ($but = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        if ($temps_de_jeu = $stmt->fetch(PDO::FETCH_ASSOC)) {
             echo "<h3>Modifier les informations du temps de jeu </h3>";
             echo "<form action='' method='post'>";
             echo "ID Joueur : <input type='number' name='id_joueur' value='" . htmlspecialchars($temps_de_jeu['id_joueur']) . "' required><br>";
             echo "ID Match : <input type='number' name='id_match' value='" . htmlspecialchars($temps_de_jeu['id_match']) . "' required><br>";
-            echo "Temps : <input type='number' name='temps' value='" . htmlspecialchars($temps_de_jeu['temps']) . "'> <br>";
+            echo "Temps : <input type='number' name='temps_de_jeu' value='" . htmlspecialchars($temps_de_jeu['temps_de_jeu']) . "'> <br>";
             echo "Numero : <input type='number' name='numero' value='" . htmlspecialchars($temps_de_jeu['numero']) . "'> <br>";
-            echo "Poste : <input type='checkbox' name='poste' " . htmlspecialchars($temps_de_jeu['poste']. "'><br>";
+            echo "Poste : <input type='text' name='poste' value='" . htmlspecialchars($temps_de_jeu['poste']) . "'> <br>";
             echo "<input type='hidden' name='id_temps_de_jeu' value='" . htmlspecialchars($temps_de_jeu['id_temps_de_jeu']) . "'>";
             echo "<input type='hidden' name='update' value='1'>";
             echo "<input type='submit' value='Mettre à jour'>";
@@ -26,16 +26,16 @@ try {
         }
     }
 
-    if (isset($_POST['id_but']) && isset($_POST['update'])) {
+    if (isset($_POST['id_temps_de_jeu']) && isset($_POST['update'])) {
         $id_temps_de_jeu = $_POST['id_temps_de_jeu'];
         $id_joueur = $_POST['id_joueur'];
         $id_match = $_POST['id_match'];
-        $temps = $_POST['temps'];
+        $temps_de_jeu = $_POST['temps_de_jeu'];
         $numero = $_POST['numero'];
         $poste = $_POST['poste'];
 
-        $sql = 'UPDATE temps_de_jeu SET id_joueur = :id_joueur, id_match = :id_match, temps = :temps, numero = :numero, poste = :poste,
-        WHERE id_temps_de jeu = :id';
+        $sql = 'UPDATE temps_de_jeu SET id_joueur = :id_joueur, id_match = :id_match, temps_de_jeu = :temps_de_jeu, numero = :numero, poste = :poste
+        WHERE id_temps_de_jeu = :id';
 
 
         $stmt = $base->prepare($sql);
@@ -43,7 +43,7 @@ try {
             'id' => $id_temps_de_jeu,
             'id_joueur' => $id_joueur,
             'id_match' => $id_match,
-            'temps' => $temps,
+            'temps_de_jeu' => $temps_de_jeu,
             'numero' => $numero,
             'poste' => $poste,
         ]);
