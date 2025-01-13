5881c96dd153db747bcf4e0d86b35688ce0d887b
MRTTBZ61 <murat.tbz@gmail.com>
2025-01-13 18:38:07 +0100

    Update modifierstade.php

--- Web Code/modifierstade.php
+++ Web Code/modifierstade.php
@@ -13,8 +13,6 @@ try {
             echo "<h3>Modifier les informations du stade</h3>";
             echo "<form action='' method='post'>";
             echo "Nom : <input type='text' name='nom' value='" . htmlspecialchars($stade['nom']) . "' required><br>";
-            echo "Ville : <input type='text' name='ville' value='" . htmlspecialchars($stade['ville']) . "' required><br>";
-            echo "Capacité : <input type='number' name='capacite' value='" . htmlspecialchars($stade['capacite']) . "' required><br>";
             echo "<input type='hidden' name='id_stade' value='" . $stade['id_stade'] . "'>";
             echo "<input type='hidden' name='update' value='1'>";
             echo "<input type='submit' value='Mettre à jour'>";
@@ -27,16 +25,12 @@ try {
     if (isset($_POST['id_stade']) && isset($_POST['update'])) {
         $id_stade = $_POST['id_stade'];
         $nom = $_POST['nom'] ?? '';
-        $ville = $_POST['ville'] ?? '';
-        $capacite = $_POST['capacite'] ?? '';
 
-        $sql = 'UPDATE stade SET nom = :nom, ville = :ville, capacite = :capacite WHERE id_stade = :id';
+        $sql = 'UPDATE stade SET nom = :nom WHERE id_stade = :id';
         $stmt = $base->prepare($sql);
         $stmt->execute([
             'id' => $id_stade,
-            'nom' => $nom,
-            'ville' => $ville,
-            'capacite' => $capacite,
+            'nom' => $nom
         ]);
 
         echo "<h3>Les informations du stade ont été mises à jour avec succès !</h3>";
